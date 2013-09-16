6d14987cd00cc7e40a38dec24a1a4aa0cb5637ae
daformat <mat.jouhet@gmail.com>
2013-09-16 17:40:30 +0200

    Add keyboard manipulation to selectable list behavior

--- web/js/selectable-list.js
+++ web/js/selectable-list.js
@@ -1,6 +1,24 @@
 $(function(){
+
 	$(".selectable li").hover(function(){
 		$('.selectable li').removeClass('active');
 		$(this).addClass('active');
-	}, function(){})
+	}, function(){
+		/* No action required */
+	})
+
+	$(document).keydown(function(e){
+	    if (e.keyCode == 38) { //up
+	      var $selected = $('.selectable li.active').removeClass('active');
+				var $choices = $selected.parent().children();
+		    $choices.eq(($choices.index($selected) - 1) % $choices.length).addClass("active");
+	      return false;
+	    }
+	    else if (e.keyCode == 40) { //down 
+	      var $selected = $('.selectable li.active').removeClass('active');
+				var $choices = $selected.parent().children();
+		    $choices.eq(($choices.index($selected) + 1) % $choices.length).addClass("active");	       
+	    }
+
+	});
 });
