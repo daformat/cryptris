440b875f47b9fb5a8a6d831c17d26e0fd4a7b2dc
daformat <mat.jouhet@gmail.com>
2013-09-19 10:07:52 +0200

    Add query string to get player name

--- web/js/scene-02.js
+++ web/js/scene-02.js
@@ -7,5 +7,5 @@ $(function(){
 
 function displaySecondDialog(){
 	$('.dialog .content .text').text('');
-	$('.dialog .content .text').typeLetterByLetter("Parfait <em>Mathieu</em>, ton compte est maintenant créé. Afin de sécuriser les échanges sur le réseau, nous utilisons un protocole de cryptographie asymétrique.", 20);
+	$('.dialog .content .text').typeLetterByLetter("Parfait <em>"+getQueryString("name","Nom du joueur")+"</em>, ton compte est maintenant créé. Afin de sécuriser les échanges sur le réseau, nous utilisons un protocole de cryptographie asymétrique.", 20);
 }
