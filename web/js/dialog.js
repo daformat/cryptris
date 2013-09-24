470e700368cf3908de394fd13383033c841b0bda
daformat <mat.jouhet@gmail.com>
2013-09-25 01:22:32 +0200

    First choice selected by default on Mutliple answer dialog

--- web/js/dialog.js
+++ web/js/dialog.js
@@ -119,15 +119,18 @@
 			var $list = $('<ul class="selectable"></ul>');
 
 			// loop through content and create links
+			var i = 0;
 	    for (key in settings.content) {
 	    	if (settings.content.hasOwnProperty(key)) {
 					var control = settings.content[key];
-		    	var $li = $('<li></li>');
+		    	var $li = $('<li class='+( (i===0) ? 'active': '')+'></li>');
 		    	var $control = $('<a href="#">'+control.label+'</a>').addClass(control.class);
 	  	  			$control.click(control.onClick);
 	  	  	
 	  	  	$li.append('<span class="arrow">▶</span>', $control, '<span class="arrow">◀</span>');
 	  	  	$list.append($li);
+
+	  	  	i++
 	  	  }
     	}
 
