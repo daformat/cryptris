9d1b95a5a3e0f1d762199335a184cbd7683c39ce
daformat <mat.jouhet@gmail.com>
2013-09-25 03:12:33 +0200

    Prevent selectable list keyboard navigation if no selectable list is found in the DOM

--- web/js/selectable-list.js
+++ web/js/selectable-list.js
@@ -8,21 +8,27 @@ $(function(){
 	$(document).keydown(function(e){
 	    if (e.keyCode == 38) { //up
 	      var $selected = $('.selectable li.active').removeClass('active');
-				var $choices = $('.selectable li');
-		    $choices.eq(($choices.index($selected) - 1) % $choices.length).addClass("active");
-	      return false;
+				if($selected.length>0) {
+					var $choices = $('.selectable li');
+		    	$choices.eq(($choices.index($selected) - 1) % $choices.length).addClass("active");	       
+					return false;	
+				}
 	    }
 	    else if (e.keyCode == 40) { // down 
 	      var $selected = $('.selectable li.active').removeClass('active');
-				var $choices = $('.selectable li');
-		    $choices.eq(($choices.index($selected) + 1) % $choices.length).addClass("active");	       
-				return false;	
+				if($selected.length>0) {
+					var $choices = $('.selectable li');
+		    	$choices.eq(($choices.index($selected) + 1) % $choices.length).addClass("active");	       
+					return false;	
+				}
 	    }
 			else if (e.keyCode == 13 || e.keyCode == 32) { // enter || space
 				var $a = $('.selectable li.active a');
-				window.location = $a.attr('href');
-				$a.click();
-				return false;
+				if($a.length>0) {
+					window.location = $a.attr('href');
+					$a.click();
+					return false;
+				}
 			}
 
 	});
