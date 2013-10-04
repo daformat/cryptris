6a0dcf80bc7cf2fb1be5e3d7126632c78ebeaf59
daformat <mat.jouhet@gmail.com>
2013-10-04 11:06:19 +0200

    Remove web folder

--- web/js/keyboard-nav.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$(document).keydown(function(e){
-		if(e.keyCode == 27){
-			var $close = $('.btn-close');
-			$close.click();
-			window.location = $close.attr('href');
-		}
-	});
-});
