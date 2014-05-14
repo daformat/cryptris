e8ae15d89f9ace6f594aa5ae3192e69552a871ab
daformat <mathieu@digitalcuisine.fr>
2014-05-14 12:55:34 +0200

    Update sharing props

--- js/jeu.js
+++ js/jeu.js
@@ -261,33 +261,39 @@ $(document).ready(function() {
 
 
 
+/**
+ * SHARING
+ */
 
+    var url         = cryptrisSettings.appUrl,
+        hrefPath    = url,
+        title       = "Cryptris, un jeu gratuit sur la cryptographie asymétrique",
+        text        = "Je viens de terminer Cryptris - un jeu sur l’univers de la cryptographie asymétrique. Affrontez l’ordinateur sur ",
+        preview     = "http://daformat.github.io/cryptris/img/cryptis-social-preview-600x600.png";
+        preview_xl  = "http://daformat.github.io/cryptris/img/cryptis-social-preview-1200x630.png";
 
-    var url = cryptrisSettings.appUrl;
-    
-    var  hrefPath = url;
+    /** Setup sharing urls **/
 
-
-     // Setup sharing urls
-    $('#share-tw').attr("href", "https://twitter.com/intent/tweet?text=Je viens de terminer Cryptris - un jeu sur l’univers de la cryptographie asymétrique. Affrontez l’ordinateur sur &url=" + url);
+    // twitter
+    $('#share-tw').attr("href", "https://twitter.com/intent/tweet?text="+text+"&url=" + url);
     $('#share-tw').attr("target", "_blank");
 
+    // google+
     $('#share-gp').attr('data-contenturl', hrefPath);
     $('#share-gp').attr('data-calltoactionurl', url);
-
+    $('#share-gp').attr('data-prefilltext', 'Vous aussi, venez essayer de battre l’ordinateur en jouant à Cryptris.');
+          
     (function() {
         var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
         po.src = 'https://apis.google.com/js/client:plusone.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
     })();
 
-    $('#share-fb').attr('href', 'http://www.facebook.com/sharer.php?s=100&p[url]=' + url + '&p[title]=Cryptris, un jeu sur la cryptographie asymétrique'+ '&p[summary]=Je viens de terminer Cryptris - un jeu sur l’univers de la cryptographie asymétrique. Jouez contre l’ordinateur pour savoir qui arrivera à décrypter le plus rapidement.');
-    $('#share-fb').attr('target', '_blank');
-    $('#share-fb').attr('onclick', 'javascript:window.open(\'http://www.facebook.com/sharer.php?s=100&p[url]='
-                                     + url 
-                                     + '&p[title]=Cryptris, un jeu sur la cryptographie asymétrique' 
-                                     + '&p[summary]=e viens de terminer Cryptris - un jeu sur l’univers de la cryptographie asymétrique. Jouez contre l’ordinateur pour savoir qui arrivera à décrypter le plus rapidement.\', \'\', \''
-                                     + 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600\'); return false;');
+    // facebook
+    var fbBase = "https://www.facebook.com/dialog/feed?&app_id=525890597495827&display=popup";
+    var fbUrl = fbBase+"&caption=" + title + "&description=" + text + "&link=" + url + "&picture=" + preview + "&redirect_uri="+cryptrisSettings.appUrl+"/merci.html";
+    $('#share-fb').attr('onclick', "javascript:window.open('"+fbUrl+"', '', 'toolbar=0,status=0,width=626,height=436');");
 
-    console.log(url);    
+
+
 });
